a8b51644fccff847856ed1de9f62e0fc7920ad8c
William Rockwood <wrockwood@tadl.org>
2015-02-09 17:25:08 -0500

    uri change
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- tadl-list-gallery.php
+++ tadl-list-gallery.php
@@ -1,7 +1,7 @@
 <?php
 /* 
     Plugin Name: TADL List Gallery
-    Plugin URI: http://tech.tadl.org/
+    Plugin URI: http://tech.tadl.org/tadl-list-gallery
     Version: v0.10
     Author: <a href="http://tech.tadl.org/">TADL Technology Team</a>
     Description: A plugin for displaying item lists on our website using a shortcode.
